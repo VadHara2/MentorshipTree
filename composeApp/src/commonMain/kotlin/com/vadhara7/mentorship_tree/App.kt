488a6990638ec72c68e96ed16c84a160a0257054
Vadym <VadHara2@gmail.com>
2025-06-16 14:42:57 +0300

    Add "Back" button to SecondScreen and navigation functionality
    This commit introduces a "Back" button to the `SecondScreen` composable.
    Clicking this button now navigates the user back to the previous screen in the navigation stack.
    
    Key changes:
    - Added a `Button` with the text "Back" to `SecondScreen`.
    - The `onClick` lambda of this button calls the `onBack` function passed as a parameter.
    - In `App.kt`, when navigating to `SecondScreen`, the `onBack` lambda is now implemented to call `navController.popBackStack()`.
    - Removed `modifier = Modifier.fillMaxSize().safeContentPadding()` from the `NavHost` in `App.kt`.

--- composeApp/src/commonMain/kotlin/com/vadhara7/mentorship_tree/App.kt
+++ composeApp/src/commonMain/kotlin/com/vadhara7/mentorship_tree/App.kt
@@ -5,27 +5,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import androidx.navigation.toRoute
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import mentorshiptree.composeapp.generated.resources.Res
 import mentorshiptree.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
@@ -33,7 +30,7 @@ fun App() {
     MaterialTheme {
         val navController = rememberNavController()
 
-        NavHost(navController = navController, startDestination = MainRouter.FirstScreen, modifier = Modifier.fillMaxSize().safeContentPadding()) {
+        NavHost(navController = navController, startDestination = MainRouter.FirstScreen) {
 
             composable<MainRouter.FirstScreen> {
                 var showContent by remember { mutableStateOf(false) }
@@ -67,7 +64,9 @@ fun App() {
 
             composable<MainRouter.SecondScreen> { backStackEntry ->
                 val args = backStackEntry.toRoute<MainRouter.SecondScreen>()
-                SecondScreen(args.text)
+                SecondScreen(args.text) {
+                    navController.popBackStack()
+                }
             }
         }
     }
