33e5fcbde14a37afcb71c935ff2066c3d6882030
Vadym <VadHara2@gmail.com>
2025-06-23 11:45:07 +0300

    Comment out `SecretsRepository`
    This commit comments out the `SecretsRepository` class in both the common and iOS-specific code.
    
    - `IOSSecretsRepository.kt`: The iOS implementation of `SecretsRepository` for retrieving the Google Auth Server ID from `Info.plist` is commented out.
    - `SecretsRepository.kt`: The common `expect class SecretsRepository` and its `getGoogleAuthServerId` method are commented out.

--- composeApp/src/iosMain/kotlin/com/vadhara7/mentorship_tree/data/repository/IOSSecretsRepository.kt
+++ composeApp/src/iosMain/kotlin/com/vadhara7/mentorship_tree/data/repository/IOSSecretsRepository.kt
@@ -2,16 +2,16 @@ package com.vadhara7.mentorship_tree.data.repository
 
 import platform.Foundation.NSBundle
 
-actual class SecretsRepository actual constructor() {
-    actual fun getGoogleAuthServerId(): String {
-
-        val info = NSBundle
-            .mainBundle
-            .infoDictionary
-            ?: error("Info.plist not found in bundle")
-
-        // Повертаємо значення під ключем "API_KEY"
-        return (info["GIDServerClientID"] as? String)
-            ?: error("GIDServerClientID not set in Info.plist")
-    }
-}
+//actual class SecretsRepository actual constructor() {
+//    actual fun getGoogleAuthServerId(): String {
+//
+//        val info = NSBundle
+//            .mainBundle
+//            .infoDictionary
+//            ?: error("Info.plist not found in bundle")
+//
+//        // Повертаємо значення під ключем "API_KEY"
+//        return (info["GIDServerClientID"] as? String)
+//            ?: error("GIDServerClientID not set in Info.plist")
+//    }
+//}
