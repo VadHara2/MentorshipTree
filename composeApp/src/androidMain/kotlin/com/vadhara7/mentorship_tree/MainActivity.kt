05ea7bf904282b2411c5e52143e2ddc8da83d326
Vadym <VadHara2@gmail.com>
2025-06-24 18:53:39 +0300

    Refactor: Enable edge-to-edge conditionally based on theme
    This commit updates `MainActivity.kt` to enable edge-to-edge display conditionally.
    The status bar style is now set to dark with a transparent background in dark theme, and light with transparent status and navigation bars in light theme.
    This change ensures that the system UI adapts appropriately to the current theme when edge-to-edge is enabled.

--- composeApp/src/androidMain/kotlin/com/vadhara7/mentorship_tree/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/vadhara7/mentorship_tree/MainActivity.kt
@@ -2,17 +2,28 @@ package com.vadhara7.mentorship_tree
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
 
         setContent {
+            enableEdgeToEdge(
+                statusBarStyle = if (isSystemInDarkTheme()) {
+                    SystemBarStyle.dark(0)
+
+                } else {
+                    SystemBarStyle.light(0, 0)
+                }
+            )
+
             App()
         }
     }
